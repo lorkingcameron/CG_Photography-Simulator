630f26c34b8ae1042273cc412540c2ac3510d484
lorkingcameron <lorkingcameron@gmail.com>
2023-03-30 01:02:49 +1100

    Initial Commit - Project Skeleton

---
+++ src/SceneManager.js
@@ -0,0 +1,51 @@
+
+
+export default class SceneManager {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this._buildScene();
+        this._buildCamera();
+        this._buildRenderer();
+    }
+
+    update() {
+        for(const object of animatedObjects) {
+            object.update();
+        }
+        renderer.render(scene, camera);
+    }
+
+    onWindowResize() {
+        const { width, height } = canvas;
+        screenDimensions.width = width;
+        screenDimensions.height = height;
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        renderer.setSize(width, height);
+    }
+
+    _buildScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xfffffff );
+    }
+
+    _buildCamera() {
+        var ratio = window.innerWidth/window.innerHeight;
+        this.camera = new THREE.PerspectiveCamera(70, ratio, 1, 1000);
+        this.camera.position.set(40,15,15);
+        this.camera.lookAt(0,0,5);
+    
+        this.scene.add(this.camera);
+    }
+
+    _buildRenderer() {
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, logarithmicDepthBuffer: true });
+        this.renderer.setSize(window.innerWidth,window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    
+        document.body.appendChild(this.renderer.domElement);
+        this.controls = new THREE.OrbitControls(this.camera,this.renderer.domElement);
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import SceneManager from "./sceneManager.js";
+
+const canvas = document.getElementById('canvas');
+const sceneManager = new SceneManager(canvas);
+window.animatedObjects = [];
+
+bindEventListeners();
+render();
+
+function bindEventListeners() {
+    window.addEventListener('resize',sceneManager.onWindowResize);
+}
+
+function render() {
+    requestAnimationFrame(render);
+    scene.update();
+}

69d52031d30924cccf784c13f1b350e50c4a471e
CianRoberts <croberts.uts@gmail.com>
2023-05-16 03:02:09 +1000

    Added a function to create noise-mapped planes.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import SceneManager from "./modules/SceneManager.js";
 
 
-import('@dimforge/rapier3d').then(RAPIER => {
-  const sceneManager = new SceneManager();
-  window.animatedObjects = [];
-  
-  sceneManager.render();
-  
-  bindEventListeners();
-  
-  function bindEventListeners() {
-      window.addEventListener('resize', () => {
-          sceneManager.onWindowResize();
-        }, false);
-  }
-})
+const sceneManager = new SceneManager();
+window.animatedObjects = [];
+
+sceneManager.render();
+
+bindEventListeners();
+
+function bindEventListeners() {
+    window.addEventListener('resize', () => {
+        sceneManager.onWindowResize();
+      }, false);
+}
+
 
